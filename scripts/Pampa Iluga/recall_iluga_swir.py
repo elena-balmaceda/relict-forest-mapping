07394be976aaf3c593affcdb71f54128ba327e64
elena-balmaceda <elenabalmacedab@gmail.com>
2025-08-14 15:55:45 -0400

    Add files via upload

---
+++ scripts/Pampa Iluga/recall_iluga_swir.py
@@ -0,0 +1,23 @@
+import pandas as pd
+ 
+  #PARA IR Y SWIR POR SEPARADO PRIMERO
+  #ANALISIS ESTADISTICA ZONAL SWIR BOSQUE ILUGA
+
+# Cargar el archivo CSV con la estadística zonal
+file_path_swir_iluga = '/Users/elenabalmaceda/Library/CloudStorage/OneDrive-UniversidadCatólicadeChile/PUBLICACIÓN TESIS EB/QGIS/ILUGA/Estadistica zonal/planillas/estadistica_zonal_swir_iluga.qmd.csv'
+df = pd.read_csv(file_path_swir_iluga)
+
+# Filtrar solo la categoría "Bosque antiguo"
+df_bosque = df[df["MC_name"] == "Bosque antiguo"]
+
+# Calcular TP (píxeles clasificados correctamente como bosque antiguo)
+TP = df_bosque["_sum"].sum()
+
+# Calcular TP + FN (píxeles totales en los ROIs de bosque antiguo)
+TP_FN = df_bosque["_count"].sum()
+
+# Calcular Recall
+recall = TP / TP_FN
+
+# Mostrar resultado
+print(f"Recall para 'Bosque antiguo': {recall:.3f} ({recall * 100:.2f}%)")
